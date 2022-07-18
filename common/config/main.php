9523bc44094f5391e4fabecc33fdc4abd0d2c542
Anton Melchakov <anton.melchakov@whitetigersoft.ru>
2022-07-18 14:31:50 +0300

    first commit

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,28 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'language' => 'ru-RU',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'parser' => [
+            'class' => 'common\components\Parser',
+            'host' => 'https://www.wildberries.ru',
+            'categoryUrl' => 'https://www.wildberries.ru/webapi/menu/main-menu-ru-ru.json',
+            'productUrl' => 'https://wbx-content-v2.wbstatic.net/ru/',
+            'productUrlv2' => 'https://card.wb.ru/cards/detail?spp=0&emp=0&locale=ru&lang=ru&curr=rub&nm=',
+            'priceHistoryUrl' => 'https://wbx-content-v2.wbstatic.net/price-history/',
+            'productListUrl' => 'https://catalog.wb.ru/catalog/',
+            'curlOpt' => [
+                'userAgent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36',
+                'header' => [
+                    'Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1',
+                    'Accept-Language: en-US,en;q=0.8,ru;q=0.6,uk;q=0.4',
+                    'Accept-Charset: Windows-1251, utf-8, *;q=0.1',
+                    'Accept-Encoding: deflate, identity, *;q=0',
+                ]
+            ]
         ],
     ],
 ];
--- console/controllers/FileController.php
+++ console/controllers/FileController.php
@@ -1,24 +1,57 @@
 <?php
 namespace console\controllers;
 
+use common\components\Parser;
 use Yii;
 use yii\console\Controller;
 use console\models\UserTextUtilForm;
 
 class FileController extends Controller
 {
-    public function actionProcess($delimiter, $mode)
+    public function actionParseCatalog()
     {
-        $form = new UserTextUtilForm();
-        $form->delimiter = $delimiter;
-        $form->mode = $mode;
-
-        if ($form->process()) {
-            $response = $form->getSuccessResponse();
-        } else {
-            $response = $form->getFirstError('');
+        /** @var Parser $dataParse */
+        $dataParse = Yii::$app->parser->loadCategories();
+        if (empty($dataParse)) {
+            $this->stdout('fail parse');
         }
+        $categoryArr = json_decode($dataParse);
+        foreach ($categoryArr as $category) {
+            if ($category->shard == "men_clothes") {
+                $this->parseChild($category);
+            }
+        }
+        $this->stdout("the end");
+    }
 
-        echo $response;
+    public function parseChild($category)
+    {
+        foreach ($category->childs as $childItem) {
+            $productResult = json_decode(Yii::$app->parser->loadProductList($childItem->shard, $childItem->query));
+            foreach ($productResult->data->products as $product) {
+                $currentPrice = Parser::getCurrentPrice($product->salePriceU, $product->priceU);
+                $lastPrice = Yii::$app->parser->getLastPrice($product->id);
+                if ($currentPrice < $lastPrice * 0.5) {
+                    $this->stdout("https://www.wildberries.ru/catalog/$product->id/detail.aspx?targetUrl=GP" . "\n");
+                }
+            }
+            if (!empty($childItem->childs)) {
+                $this->parseChild($childItem);
+            }
+        }
+    }
+
+    public function actionParseProduct()
+    {
+        /** @var Parser $dataParse */
+        $dataParse = Yii::$app->parser->loadProductPriceHistory(37503888);
+        if (empty($dataParse)) {
+            $this->stdout('fail parse');
+        }
+        $priceArr = json_decode($dataParse);
+        foreach ($priceArr as $priceItem) {
+            $this->stdout(Yii::$app->formatter->asDate($priceItem->dt) . " " . (int)$priceItem->price->RUB / 100 . "\n");
+        }
+        die;
     }
 }
